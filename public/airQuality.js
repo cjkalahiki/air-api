193f8fd6165aa9eeba5894aa14d4a57341d15a6e
Connor Kalahiki <connorkalahiki@gmail.com>
2021-02-17 14:09:10 -0800

    submission commit

--- public/airQuality.js
+++ public/airQuality.js
@@ -2,8 +2,19 @@ const baseURL = 'http://api.airvisual.com/v2/'; //baseURL + endpoint?key=${key}
 const key = '2277dd3e-931a-4647-9939-2f1975107712';
 let url; //declare url
 
-//button
+//elements
 const submitButton = document.querySelector('.submitButton');
+const card = document.querySelector('.weatherCard');
+const weatherList = document.querySelector('.weatherList');
+const temperature = document.querySelector('.temp');
+const humidity = document.querySelector('.humidity');
+const airQuality = document.querySelector('.airQuality');
+const windSpeed = document.querySelector('.windSpeed');
+const weatherIcon = document.querySelector('.weatherIcon');
+const weatherHeader = document.querySelector('.weatherHeader');
+
+let initialStyle = card.style.display;
+card.style.display = 'none';
 
 //eventListener
 submitButton.addEventListener('click', fetchCountry);
@@ -19,18 +30,64 @@ function fetchCountry(e) {
         })
         .then(function(result){
             console.log(result);
+            displayResults(result);
         })
         .catch(error => console.log('error', error));
 }
 
-function update(term){ //this changes searches to single all caps string
-    //need conditionals set up to check if there are spaces
-    // term.forEach(function(letter){
-    //     if (letter === ' '){
+function displayResults(json) {
+    while (weatherList.firstChild){
+        weatherList.removeChild(weatherList.firstChild);
+    }
 
-    //     }
-    // })
-    term = term.toUpperCase();
-    term = term.split(' ');
-    term = term.join('_');
+    card.style.display = initialStyle;
+
+    let temper = document.createElement('li');
+    temper.className = 'temp list-group-item';
+    let humid = document.createElement('li');
+    humid.className = 'humidity list-group-item';
+    let airqual = document.createElement('li');
+    airqual.className = 'airQuality list-group-item';
+    let wind = document.createElement('li');
+    wind.className = 'windSpeed list-group-item';
+    let smallHeading = document.createElement('small');
+    smallHeading.className = 'text-muted';
+    let breakEl = document.createElement('br');
+    
+
+    let data = json.data;
+    let city = data.city;
+    let country = data.country;
+    let weather = data.current.weather;
+
+    let temp = weather.tp;
+    let hu = weather.hu;
+    let windy = weather.ws;
+    let quality = data.current.pollution.aqius;
+    let fullDate = weather.ts;
+    let year = fullDate.split('-')[0];
+    let month = fullDate.split('-')[1];
+    let day = fullDate.split('-')[2][0] + fullDate.split('-')[2][0];
+
+
+
+    let image = weather.ic;
+    weatherIcon.src = `./assets/${image}.png`;
+
+    temper.textContent = `Temperature (C°): ${temp}`;
+    humid.textContent = `Humidity: ${hu}`;
+    airqual.textContent = `Air Quality: ${quality}`;
+    wind.textContent = `Wind Speed (mph): ${windy}`;
+    smallHeading.textContent = `${city}, ${country}`;
+    weatherHeader.textContent = `${month}/${day}/${year}`;
+
+    //let year = 
+    // weatherHeader.textContent = `${date}` + year 
+
+    weatherList.appendChild(temper);
+    weatherList.appendChild(humid);
+    weatherList.appendChild(airqual);
+    weatherList.appendChild(wind);
+    weatherHeader.appendChild(breakEl);
+    weatherHeader.appendChild(smallHeading);
 }
